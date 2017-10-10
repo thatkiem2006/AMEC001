dd56b08168cc20e5e1b254d67fc2ccb6ae2a0365
linhth <linhtranhai87@gmail.com>
2017-10-10 13:09:11 +0700

    first commit

---
+++ amecCode/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  amecCode
+//
+//  Created by linhth on 10/10/17.
+//  Copyright © 2017 adv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+ 
+
+}
+
